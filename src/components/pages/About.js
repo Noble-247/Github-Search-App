eb077674f4e8449a3cfcb9f6b85ba2bfc566c42e
Emmanuel Ekpobimi <noble4special247@gmail.com>
2021-06-24 20:09:31 +0100

    Added @emotion/css library,

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,8 +1,14 @@
+import { css } from "@emotion/css";
 import React from "react";
 
 const About = () => {
   return (
-    <div style={aboutPageStyles}>
+    <div
+      className={css`
+        line-height: 1.5;
+        margin-bottom: 65.5vh;
+      `}
+    >
       <h1>About This App</h1>
       <p>This App can be used to search Github users</p>
       <p>Version: 1.0.0</p>
@@ -10,8 +16,4 @@ const About = () => {
   );
 };
 
-const aboutPageStyles = {
-  lineHeight: "1.5",
-  marginBottom: "65.5vh",
-};
 export default About;
--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,26 +1,29 @@
+import { css } from "@emotion/css";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className='container text-center' style={NotFoundStyles}>
-      <p className='text-uppercase large'>
-        SORRY, LOOKS LIKE WE SENT YOU THE WRONG WAY{" "}
-      </p>
-      <p className='text-capitalize lead'>Let us guide you back</p>
+    <div className='container text-center'>
+      <div
+        className={css`
+          margin-top: 10vh;
+          margin-bottom: 39.1vh;
+          margin-left: 5rem;
+          margin-right: 5rem;
+        `}
+      >
+        <p className='text-uppercase large'>
+          SORRY, LOOKS LIKE WE SENT YOU THE WRONG WAY{" "}
+        </p>
+        <p className='text-capitalize lead'>Let us guide you back</p>
 
-      <Link to='/' className='btn btn-primary'>
-        Head back to the homepage...
-      </Link>
+        <Link to='/' className='btn btn-primary'>
+          Head back to the homepage...
+        </Link>
+      </div>
     </div>
   );
-};
-
-const NotFoundStyles = {
-  marginTop: "10vh",
-  marginBottom: "39.1vh",
-  marginLeft: "5rem",
-  marginRight: "5rem",
 };
 
 export default NotFound;
